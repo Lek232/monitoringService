d483b435b44747c1f500e3ae644697addf2100ef
Oleg Tolkachev <olega232@gmail.com>
2026-01-20 16:03:55 +0300

    first commit

--- src/main/kotlin/com/test/monitoringService/MonitoringServiceApplication.kt
+++ src/main/kotlin/com/test/monitoringService/MonitoringServiceApplication.kt
@@ -1,11 +1,9 @@
 package com.test.monitoringService
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-@EnableConfigurationProperties
 class MonitoringServiceApplication
 
 fun main(args: Array<String>) {
